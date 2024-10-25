fb630478592b086ea819416e2e8c0f0a8fa457f1
Rizki22-017 <126841212+Rizki22-017@users.noreply.github.com>
2024-10-25 14:04:30 +0700

    update nik

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -180,7 +180,7 @@
     <script src={{asset('assets/vendor/isotope-layout/isotope.pkgd.min.js')}}></script>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"></script>
+    
 
 
     <!-- Main JS File -->
