aae18dd02cf2de6859bf24ed9cf6593857805450
Eter <famutashiro@gmail.com>
2024-10-25 13:51:52 +0700

    js-update

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -9,8 +9,8 @@
     <meta name="keywords" content="">
 
     <!-- Favicons -->
-    <link href={{asset('assets/img/favicon.png')}} rel="icon">
-    <link href={{asset('assets/img/apple-touch-icon.png')}} rel="apple-touch-icon">
+    <link href={{ asset('assets/img/favicon.png') }} rel="icon">
+    <link href={{ asset('assets/img/apple-touch-icon.png') }} rel="apple-touch-icon">
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com" rel="preconnect">
@@ -20,11 +20,11 @@
         rel="stylesheet">
 
     <!-- Vendor CSS Files -->
-    <link href={{asset('assets/vendor/bootstrap/css/bootstrap.min.css')}} rel="stylesheet">
-    <link href={{asset('assets/vendor/bootstrap-icons/bootstrap-icons.css')}} rel="stylesheet">
-    <link href={{asset('assets/vendor/aos/aos.css')}} rel="stylesheet">
-    <link href={{asset('assets/vendor/swiper/swiper-bundle.min.css')}} rel="stylesheet">
-    <link href={{asset('assets/vendor/glightbox/css/glightbox.min.css')}} rel="stylesheet">
+    <link href={{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }} rel="stylesheet">
+    <link href={{ asset('assets/vendor/bootstrap-icons/bootstrap-icons.css') }} rel="stylesheet">
+    <link href={{ asset('assets/vendor/aos/aos.css') }} rel="stylesheet">
+    <link href={{ asset('assets/vendor/swiper/swiper-bundle.min.css') }} rel="stylesheet">
+    <link href={{ asset('assets/vendor/glightbox/css/glightbox.min.css') }} rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Main CSS File -->
@@ -170,21 +170,21 @@
     <div id="preloader"></div>
 
     <!-- Vendor JS Files -->
-    <script src={{asset('assets/vendor/php-email-form/validate.js')}}></script>
-    <script src={{asset('assets/vendor/aos/aos.js')}}></script>
-    <script src={{asset('assets/vendor/purecounter/purecounter_vanilla.js')}}></script>
-    <script src={{asset('assets/vendor/waypoints/noframework.waypoints.js')}}></script>
-    <script src={{asset('assets/vendor/swiper/swiper-bundle.min.js')}}></script>
-    <script src={{asset('assets/vendor/glightbox/js/glightbox.min.js')}}></script>
-    <script src={{asset('assets/vendor/imagesloaded/imagesloaded.pkgd.min.js')}}></script>
-    <script src={{asset('assets/vendor/isotope-layout/isotope.pkgd.min.js')}}></script>
+    <script src={{ asset('assets/vendor/php-email-form/validate.js') }}></script>
+    <script src={{ asset('assets/vendor/aos/aos.js') }}></script>
+    <script src={{ asset('assets/vendor/purecounter/purecounter_vanilla.js') }}></script>
+    <script src={{ asset('assets/vendor/waypoints/noframework.waypoints.js') }}></script>
+    <script src={{ asset('assets/vendor/swiper/swiper-bundle.min.js') }}></script>
+    <script src={{ asset('assets/vendor/glightbox/js/glightbox.min.js') }}></script>
+    <script src={{ asset('assets/vendor/imagesloaded/imagesloaded.pkgd.min.js') }}></script>
+    <script src={{ asset('assets/vendor/isotope-layout/isotope.pkgd.min.js') }}></script>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"></script>
 
 
     <!-- Main JS File -->
-    <script src="assets/js/main.js"></script>
+    <script src="{{ asset('assets/js/main.js') }}"></script>
 
 
 </body>
