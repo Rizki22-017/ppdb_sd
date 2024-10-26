d9c1c08d05c3e9d20c91b05d7ad6f371807a3da1
Rizki22-017 <126841212+Rizki22-017@users.noreply.github.com>
2024-10-26 15:08:31 +0700

    update lagi

--- resources/views/steptwo.blade.php
+++ resources/views/steptwo.blade.php
@@ -28,10 +28,9 @@
                 </div>
 
                 <div class="tab-content" data-aos="fade-up" data-aos-delay="200">
-                    <form action="{{ route('step2.post', $register_id) }}" method="POST" id="formdata-step2">
+                    <form action="{{ route('step2.post', $register_id) }}" method="POST" id="step2Form">
                         @csrf
-                        <input type="hidden" name="step" value="2">
-                        <input type="hidden" name="user_id" value="{{ optional($registration)->user_id }}">
+
                         <!-- Parent Information Section -->
                         <div class="row">
                             <div class="col-lg-6">
