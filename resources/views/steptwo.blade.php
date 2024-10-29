3b2e9c690cf462dd4ff219ca2df78b9266d70422
Rizki22-017 <126841212+Rizki22-017@users.noreply.github.com>
2024-10-29 13:35:49 +0700

    steptwo-update

--- resources/views/steptwo.blade.php
+++ resources/views/steptwo.blade.php
@@ -50,7 +50,7 @@
                                             <label for="nikAyah" class="form-label">NIK Ayah<span
                                                     class="text-danger">*</span></label>
                                             <input type="text" class="form-control" id="nikAyah" name="nik_ayah"
-                                            value="{{ old('nik_ayah', $registration->nik_ayah ?? '') }}"
+                                            value="{{ old('nik_ayah', $registration->nik_ayah ?? '')}}"
                                             required placeholder="Masukan NIK ayah" maxlength="16" pattern="\d{16}"
                                                 title="NIK harus 16 digit angka">
                                             <span class="error-message"></span> <!-- Ini untuk pesan kesalahan -->
@@ -59,7 +59,7 @@
                                             <label for="namaLengkapIbu" class="form-label">Nama Lengkap Ibu <span
                                                     class="text-danger">*</span></label>
                                             <input type="text" class="form-control" id="namaLengkapIbu"
-                                                name="nama_lengkap_ibu" value="{{ old('nama_lengkap_bu', $registration->nama_lengkap_ibu ?? '') }}"
+                                                name="nama_lengkap_ibu" value="{{ old('nama_lengkap_bu', $registration->nama_lengkap_ibu ?? '')}}"
                                                 required placeholder="Masukan nama lengkap ayah">
                                             <span class="error-message"></span> <!-- Ini untuk pesan kesalahan -->
                                         </div>
@@ -67,7 +67,7 @@
                                             <label for="nikAyah" class="form-label">NIK Ibu<span
                                                     class="text-danger">*</span></label>
                                             <input type="text" class="form-control" id="nikIbu" name="nik_ibu"
-                                                value="{{ old('nik_ibu', $registration->nik_ibu ?? '') }}"
+                                                value="{{ old('nik_ibu', $registration->nik_ibu ?? '')}}"
                                                 required placeholder="Masukan NIK ayah" maxlength="16" pattern="\d{16}"
                                                 title="NIK harus 16 digit angka">
                                             <span class="error-message"></span> <!-- Ini untuk pesan kesalahan -->
@@ -136,12 +136,12 @@
                                         <div class="row">
                                             <div class="col-12 col-md-6 mb-2">
                                                 <input type="text" class="form-control" id="tempatLahirA"
-                                                    name="tempat_lahir_ayah" value="{{ old('tempat_lahir_ayah', $registration->tempat_lahir_ayah ?? '') }}"
+                                                    name="tempat_lahir_ayah" value="{{ old('tempat_lahir_ayah', $registration->tempat_lahir_ayah ?? '')}}"
                                                     required placeholder="Masukan tempat lahir ayah">
                                             </div>
                                             <div class="col-12 col-md-6 mb-2">
                                                 <input type="date" class="form-control" id="tanggalLahirA"
-                                                    name="tanggal_lahir_ayah" value="{{ old('tanggal_lahir_ayah', $registration->tanggal_lahir_ayah ?? '') }}"
+                                                    name="tanggal_lahir_ayah" value="{{ old('tanggal_lahir_ayah', $registration->tanggal_lahir_ayah ?? '')}}"
                                                     required>
                                             </div>
                                         </div>
@@ -153,12 +153,12 @@
                                         <div class="row">
                                             <div class="col-12 col-md-6 mb-2">
                                                 <input type="text" class="form-control" id="tempatLahirI"
-                                                    name="tempat_lahir_ibu" value="{{ old('tempat_lahir_ibu', $registration->tempat_lahir_ibu ?? '') }}" required
+                                                    name="tempat_lahir_ibu" value="{{ old('tempat_lahir_ibu', $registration->tempat_lahir_ibu ?? '')}}" required
                                                     placeholder="Masukan tempat lahir ibu">
                                             </div>
                                             <div class="col-12 col-md-6 mb-2">
                                                 <input type="date" class="form-control" id="tanggalLahirI"
-                                                    name="tanggal_lahir_ibu" value="{{ old('tanggal_lahir_ibu', $registration->tabggal_lahir_ibu ?? '') }}" required>
+                                                    name="tanggal_lahir_ibu" value="{{ old('tanggal_lahir_ibu', $registration->tabggal_lahir_ibu ?? '')}}" required>
                                             </div>
                                         </div>
                                         <span class="error-message"></span> <!-- Ini untuk pesan kesalahan -->
@@ -168,7 +168,7 @@
                                         <label for="alamat" class="form-label">Alamat Rumah<span
                                                 class="text-danger">*</span></label>
                                         <input type="text" class="form-control" id="alamat" name="alamat_ortu"
-                                            value="{{ old('alamat_ortu', $registration->alamat_ortu ?? '') }}"
+                                            value="{{ old('alamat_ortu', $registration->alamat_ortu ?? '')}}"
                                             required placeholder="Contoh : Jl. Sudirman No. 00, Kota Padang">
                                         <span class="error-message"></span>
                                     </div>
@@ -177,7 +177,7 @@
                                         <label for="kodePos" class="form-label">Kode Pos<span
                                                 class="text-danger">*</span></label>
                                         <input type="number" class="form-control" id="kodepos" name="kode_pos_ortu"
-                                            value="{{ old('kode_pos_ortu', $registration->kode_pos_ortu ?? '') }}"
+                                            value="{{ old('kode_pos_ortu', $registration->kode_pos_ortu ?? '')}}"
                                             required placeholder="Masukan kode pos anda">
                                         <span class="error-message"></span>
                                     </div>
@@ -186,7 +186,7 @@
                                         <label for="notelp" class="form-label">Nomor Telepon<span
                                                 class="text-danger">*</span></label>
                                         <input type="tel" class="form-control" id="notelp" name="no_telp_ortu"
-                                            value="{{ old('no_telp_ortu', $registration->no_telp_ortu ?? '') }}"
+                                            value="{{ old('no_telp_ortu', $registration->no_telp_ortu ?? '')}}"
                                             required placeholder="Contoh : 6282123456789">
                                         <span class="error-message"></span>
                                     </div>
@@ -198,7 +198,7 @@
                                                     class="text-danger">*</span></label>
                                             <div class="col-sm-10">
                                                 <input type="text" class="form-control" id="kantorAyah"
-                                                    name="kantor_ayah" value="{{ old('kantor_ayah', $registration->kantor_ayah ?? '') }}"
+                                                    name="kantor_ayah" value="{{ old('kantor_ayah', $registration->kantor_ayah ?? '')}}"
                                                     required placeholder="Jl. Sudirman, Kota Padang">
                                                 <span class="error-message"></span> <!-- Ini untuk pesan kesalahan -->
                                             </div>
@@ -208,7 +208,7 @@
                                                     class="text-danger">*</span></label>
                                             <div class="col-sm-10">
                                                 <input type="text" class="form-control" id="kantorIbu"
-                                                    name="kantor_ibu" value="{{ old('kantor_ibu', $registration->kantor_ibu ?? '') }}"
+                                                    name="kantor_ibu" value="{{ old('kantor_ibu', $registration->kantor_ibu ?? '')}}"
                                                     required placeholder="Jl. Sudirman, Kota Padang">
                                                 <span class="error-message"></span> <!-- Ini untuk pesan kesalahan -->
                                             </div>
@@ -224,7 +224,7 @@
                                                     class="text-danger">*</span></label>
                                             <div class="col-sm-10">
                                                 <input type="tel" class="form-control" id="nohpAyah"
-                                                    name="no_hp_ayah" value="{{ old('no_hp_ayah', $registration->no_hp_ayah ?? '') }}"
+                                                    name="no_hp_ayah" value="{{ old('no_hp_ayah', $registration->no_hp_ayah ?? '')}}"
                                                     required placeholder="Contoh : 6282123456789">
                                                 <span class="error-message"></span> <!-- Ini untuk pesan kesalahan -->
                                             </div>
@@ -234,7 +234,7 @@
                                                     class="text-danger">*</span></label>
                                             <div class="col-sm-10">
                                                 <input type="tel" class="form-control" id="nohpIbu"
-                                                    name="no_hp_ibu" value="{{ old('no_hp_ibu', $registration->no_hp_ibu ?? '') }}"
+                                                    name="no_hp_ibu" value="{{ old('no_hp_ibu', $registration->no_hp_ibu ?? '')}}"
                                                     required placeholder="Contoh : 6282123456789">
                                                 <span class="error-message"></span> <!-- Ini untuk pesan kesalahan -->
                                             </div>
@@ -361,7 +361,7 @@
                                             </div>
                                             <input type="text" class="form-control mt-2"
                                                 id="inputLainpendidikan_ayah_lain" name="pendidikan_ayah_lain"
-                                                value="{{ old('pendidikan_ayah_lain', $registration->pendidikan_ayah_lain?? '') }}"
+                                                value="{{ old('pendidikan_ayah_lain', $registration->pendidikan_ayah_lain??'')}}"
                                                 placeholder="Sebutkan..." style="display:none;">
                                         </div>
                                     </div>
@@ -443,7 +443,7 @@
                                                 <option value="Other">Lain-Lain, sebutkan</option>
                                             </select>
                                             <input type="text" class="form-control mt-2" id="detailInputAyah"
-                                                name="pekerjaan_ayah_detail" value="{{ old('pekerjaan_ayah_detail', $registration->pekerjaan_ayah_detail ?? '') }}"
+                                                name="pekerjaan_ayah_detail" value="{{ old('pekerjaan_ayah_detail', $registration->pekerjaan_ayah_detail ?? '')}}"
                                                 placeholder="Masukan detail golongan"
                                                 style="display: none;">
                                         </div>
@@ -465,7 +465,7 @@
                                                 <option value="Other">Lain-Lain, sebutkan</option>
                                             </select>
                                             <input type="text" class="form-control mt-2" id="detailInputIbu"
-                                                name="pekerjaan_ibu_detail" value="{{ old('pekerjaan_ibu_detail', $registration->pekerjaan_ibu_detail ?? '') }}"
+                                                name="pekerjaan_ibu_detail" value="{{ old('pekerjaan_ibu_detail', $registration->pekerjaan_ibu_detail ?? '')}}"
                                                 placeholder="Masukan detail golongan"
                                                 style="display: none;">
                                         </div>
@@ -506,7 +506,7 @@
                                                 class="text-danger">*</span></label>
                                         <div class="col-sm-10">
                                             <input type="tel" class="form-control" id="jumlahTanggungan"
-                                                name="jumlah_tanggungan" value="{{ old('jumlah_tanggungan', $registration->jumlah_tanggungan ?? '') }}">
+                                                name="jumlah_tanggungan" value="{{ old('jumlah_tanggungan', $registration->jumlah_tanggungan ?? '')}}">
                                             <span class="error-message"></span> <!-- Ini untuk pesan kesalahan -->
                                         </div>
                                     </div>
@@ -572,22 +572,22 @@
                                     <div class="mb-3">
                                         <label for="namaLengkapTg" class="form-label">Nama Lengkap Tanggungan</label>
                                         <input type="text" class="form-control" id="namaLengkapTg" name="nama_lengkap_tanggungan"
-                                        value="{{ old('nama_lengkap_tanggungan', $registration->nama_lengkap_tanggungan ?? '') }}">
+                                        value="{{ old('nama_lengkap_tanggungan', $registration->nama_lengkap_tanggungan ?? '')}}">
                                     </div>
                                     <div class="mb-3">
                                         <label for="sekolahTg" class="form-label">Sekolah/Kampus</label>
                                         <input type="text" class="form-control" id="sekolahTg" name="sekolah_tanggungan"
-                                        value="{{ old('sekolah_tanggungan', $registration->sekolah_tanggungan ?? '') }}">
+                                        value="{{ old('sekolah_tanggungan', $registration->sekolah_tanggungan ?? '')}}">
                                     </div>
                                     <div class="mb-3">
                                         <label for="kelasTg" class="form-label">Kelas/Semester</label>
                                         <input type="text" class="form-control" id="kelasTg" name="kelas_tanggungan"
-                                        value="{{ old('kelas_tanggungan', $registration->kelas_tanggungan ?? '') }}">
+                                        value="{{ old('kelas_tanggungan', $registration->kelas_tanggungan ?? '')}}">
                                     </div>
                                     <div class="mb-3">
                                         <label for="uangSekolahTg" class="form-label">Uang Sekolah</label>
                                         <input type="text" class="form-control" id="uangSekolahTg" name="uang_sekolah_tanggungan"
-                                        value="{{ old('uang_sekolah_tanggungan', $registration->uang_sekolah_tanggungan ?? '') }}">
+                                        value="{{ old('uang_sekolah_tanggungan', $registration->uang_sekolah_tanggungan ?? '')}}">
                                     </div>
                                     <div class="mb-3">
                                         <label for="keteranganTg" class="form-label">Keterangan</label>
